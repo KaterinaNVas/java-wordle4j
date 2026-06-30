c5426d3a6728a9b53be51c7071a82201de5f6a65
KaterinaNVas <leb.kati@yandex.ru>
2026-06-30 15:40:54 +0300

    feat: добавить WordleGame с игровой логикой
    - Конструктор принимает словарь, загаданное слово и количество попыток
    - makeGuess() обрабатывает ход игрока:
      * Проверка длины слова
      * Проверка наличия слова в словаре
      * Проверка, не использовалось ли слово
      * Подсчёт статусов букв (correct/present/absent)
      * Проверка на победу/поражение
    - Геттеры для всех полей (защищённые)
    - Методы для изменения состояния игры
    - Использует нормализацию через WordleDictionary.normalizeString()
    - Использует getLetterStatus() для проверки букв
    
    Обновлены исключения:
    - InvalidWordLengthException: добавлен конструктор с ожидаемой и фактической длиной
    - WordAlreadyUsedException: добавлен конструктор со словом

--- src/ru/yandex/practicum/WordleGame.java
+++ src/ru/yandex/practicum/WordleGame.java
@@ -1,5 +1,12 @@
 package ru.yandex.practicum;
 
+import ru.yandex.practicum.exception.InvalidWordLengthException;
+import ru.yandex.practicum.exception.WordAlreadyUsedException;
+import ru.yandex.practicum.exception.WordNotFoundInDictionaryException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 в этом классе хранится словарь и состояние игры
     текущий шаг
@@ -13,11 +20,108 @@ package ru.yandex.practicum;
 не забудьте про специальные типы исключений для игровых и неигровых ошибок
  */
 public class WordleGame {
+    private final String answer;                  // загаданное слово
+    private final int maxAttempts;                // максимум попыток
+    private int steps;                            // сколько попыток сделано
+    private final WordleDictionary dictionary;    // словарь
+    private final List<String> usedWords;       // использованные слова
+    private boolean gameFinished;                 // игра завершена?
+    private boolean gameWon;                      // игра выиграна?
 
-    private String answer;
 
-    private int steps;
+    public WordleGame(WordleDictionary dictionary, String answer, int maxAttempts) {
+        this.dictionary = dictionary;
+        this.answer = WordleDictionary.normalizeString(answer);
+        this.maxAttempts = maxAttempts;
+        this.steps = 0;
+        this.usedWords = new ArrayList<>();
+        this.gameFinished = false;
+        this.gameWon = false;
 
-    private WordleDictionary dictionary;
+    }
 
+
+    public String[] makeGuess(String guess) throws InvalidWordLengthException,
+            WordNotFoundInDictionaryException,
+            WordAlreadyUsedException {
+        if (guess.length() != getAnswer().length()) {
+            throw new InvalidWordLengthException(getAnswer().length(), guess.length());
+        }
+
+        if (!getDictionary().contains(guess)) {
+            throw new WordNotFoundInDictionaryException(guess);
+        }
+
+        if (getUsedWords().contains(guess)) {
+            throw new WordAlreadyUsedException(guess);
+        }
+
+        addUsedWord(guess);
+        incrementSteps();
+
+        String[] result = new String[getAnswer().length()];
+
+        for (int i = 0; i < getAnswer().length(); i++) {
+            result[i] = WordleDictionary.getLetterStatus(getAnswer(), guess, i);
+        }
+        if (guess.equals(getAnswer())) {
+            setGameWon(true);
+            setGameFinished(true);
+            return result;
+        }
+
+        if (getSteps() >= getMaxAttempts()) {
+            setGameWon(false);
+            setGameFinished(true);
+        }
+
+        return result;
+    }
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public void setGameFinished(boolean gameFinished) {
+        this.gameFinished = gameFinished;
+    }
+
+    public void setGameWon(boolean gameWon) {
+        this.gameWon = gameWon;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public int getMaxAttempts() {
+        return maxAttempts;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public WordleDictionary getDictionary() {
+        return dictionary;
+    }
+
+    public List<String> getUsedWords() {
+        return new ArrayList<>(usedWords);
+    }
+
+    public boolean isGameFinished() {
+        return gameFinished;
+    }
+
+    public boolean isGameWon() {
+        return gameWon;
+    }
+
+    public void addUsedWord(String word) {
+        usedWords.add(word);
+    }
+
+    public void incrementSteps() {
+        steps++;
+    }
 }
--- src/ru/yandex/practicum/exception/WordAlreadyUsedException.java
+++ src/ru/yandex/practicum/exception/WordAlreadyUsedException.java
@@ -1,4 +1,24 @@
 package ru.yandex.practicum.exception;
 
 public class WordAlreadyUsedException extends Exception{
+    public WordAlreadyUsedException() {
+    }
+
+    public WordAlreadyUsedException(String word) {
+        super("Слово '" + word + "' уже было использовано!");
+    }
+
+
+
+    public WordAlreadyUsedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WordAlreadyUsedException(Throwable cause) {
+        super(cause);
+    }
+
+    public WordAlreadyUsedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
