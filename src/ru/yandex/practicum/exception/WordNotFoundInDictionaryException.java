e48d7d39ef8bbacc71db223f2e77a76eb21caf35
KaterinaNVas <leb.kati@yandex.ru>
2026-07-02 00:46:41 +0300

    fix: исправить WhitespaceAround и EmptyLineSeparator в WordNotFoundInDictionaryException

--- src/ru/yandex/practicum/exception/WordNotFoundInDictionaryException.java
+++ src/ru/yandex/practicum/exception/WordNotFoundInDictionaryException.java
@@ -1,10 +1,11 @@
-
 package ru.yandex.practicum.exception;
 
-public class WordNotFoundInDictionaryException extends Exception{
+public class WordNotFoundInDictionaryException extends Exception {
+
     public WordNotFoundInDictionaryException(String word) {
         super("Слово '" + word + "' не найдено в словаре");
     }
+
     public WordNotFoundInDictionaryException(String message, Throwable cause) {
         super(message, cause);
     }
